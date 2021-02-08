6db644e21762159f25b79961e9b5b2c494b6b273
nazar.oprysk <nazar.oprysk@empirichealth.com>
2021-02-07 22:18:12 -0700

    feat: CI/CD process

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'fontsource-roboto';
 import './index.css';
 import App from './App';
+import SignIn from './pages/SignIn/SignIn';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SignIn />
   </React.StrictMode>,
   document.getElementById('root')
 );
